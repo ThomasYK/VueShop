cd289b53d3edbcf328f498b3bba5b6ff725c6048
ThomasYoung <787370017@qq.com>
2020-05-10 16:32:31 +0800

    电商demo

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    publicPath:'/' , //配置根目录
+    outputDir:'dist', //构建输出目录
+    assetsDir:"assets", //静态资源目录（js,css,image）
+    lintOnSave:false, // 是否开启eslint检测 false不开启，有效值：true || false
+    productionSourceMap: true,//生产环境下面开启sourceMap支持断点调试 默认开启
+    devServer:{
+        open:true, //是否启动打开浏览器
+        host:"localhost",//主机，0.0.0.0支持局域网地址，可以用真机测试
+        port:8080, //端口
+        https:false,//是否启动https
+        //配置跨域代理   不符合同源策略（同协议，同域名，同端口）的就会跨域
+        proxy:{
+            "/api":{
+            target:"http://vueshop.glbuys.com/api",
+            changeOrigin:true,
+            pathRewrite:{
+                    '^/api':""
+                }
+            }
+        }
+        },
+    // configureWebpack:{
+    //     devtool: 'source-map' //配置开发者环境的sourceMap用于断点调试
+    // }
+        
+        
+
+
+}
